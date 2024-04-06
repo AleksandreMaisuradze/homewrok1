c4e3897092060c368feb82a991cfb8ceb122ce87
GiorgiDzirkvelishvili <giorgidzirkvelishvili@hollandandbarrett.com>
2024-04-06 23:31:58 +0400

    change value state

--- homework6/src/pages/Home.js
+++ homework6/src/pages/Home.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import CountrySelector from "../components/CountrySelector";
 import { useNavigate } from "react-router-dom";
 import routes from "../constants/route";
 
 function Home() {
-    const navigate = useNavigate()
-    const clickHandler = () => {
-        navigate(routes.universities)
-    }
-    return (
-        <>
-            <div>Select country to reach universities</div>
-            <CountrySelector />
-            <button onClick={clickHandler}>Submit</button>
-        </>
-    )
+  const [country, setCountry] = useState({ label: "", value: "" });
+  const navigate = useNavigate();
+  const clickHandler = () => {
+    navigate(`${routes.universities}/${country?.label}`);
+  };
+
+  return (
+    <>
+      <div>Select country to reach universities</div>
+      <CountrySelector value={country} setValue={setCountry} />
+      <button onClick={clickHandler}>Submit</button>
+    </>
+  );
 }
 export default Home;
