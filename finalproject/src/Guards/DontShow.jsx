27a86686bce3b97dfe60de6b9d31826e27b64975
Aleksandre Maisuradze <aleksandremaisuradze11@gmail.com>
2024-05-14 13:52:27 +0400

    change DontShow return

--- finalproject/src/Guards/DontShow.jsx
+++ finalproject/src/Guards/DontShow.jsx
@@ -1,38 +1,17 @@
-import { useNavigate } from "react-router-dom"
 import { useAppContext } from "../context/AppContextProvider"
-import appRoutes from "../constants/appRoutes"
-import Button from 'react-bootstrap/Button';
 
 
 function DontShow({ children }) {
     const { state } = useAppContext()
-    const navigate = useNavigate()
-
-    const clickHandler = () => {
-        navigate(appRoutes.Registration)
-    }
-
-    const logInClickHandler = () => {
-        navigate(appRoutes.LogIn)
-    }
-
-
+    
     if (!state.isUserLoggedIn) {
 
         return <>{children}</>
     } else {
         return (
-            <div>
-                <div className="logInDiv" >
-                    <p>Please log in to see movies</p>
-                    <Button variant="primary" className="registrationButton" onClick={logInClickHandler} >Log in</Button>
-
-                </div>
-                <div className="registerDiv" >
-                    <p>If you don`t have account register </p>
-                    <Button variant="primary" className="registrationButton" onClick={clickHandler} >Register Now</Button>
-                </div>
-            </div>
+            <p style={{ fontSize: "42px", fontWeight: "bold", textAlign: "center" }}>
+                You have already logged in ore registered
+            </p>
         )
     }
 
